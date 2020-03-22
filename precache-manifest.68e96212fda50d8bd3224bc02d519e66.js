04a2ae4e5f710773a81638d008e659465ce166d5
i_am_yevhen <yevheniiperekopskyi@gmail.com>
2020-03-22 11:32:49 +0300

    Updates

---
+++ precache-manifest.68e96212fda50d8bd3224bc02d519e66.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40762a68c9a3f07bb76dc2b2a2ea4557",
+    "url": "/ReduxCounter/index.html"
+  },
+  {
+    "revision": "fa7adcf4b977866c74f5",
+    "url": "/ReduxCounter/static/js/main.9855094d.chunk.js"
+  },
+  {
+    "revision": "dc272f66e43d3038d23d",
+    "url": "/ReduxCounter/static/js/runtime-main.d5647fce.js"
+  }
+]);
